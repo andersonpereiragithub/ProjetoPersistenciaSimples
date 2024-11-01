30c9d4e8a8d5ad41f8a77996c148c503b3faa1e1
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-10-31 22:13:47 -0300

    Criado as camadas CONTROLLER e DATA

---
+++ Controller/ClienteController.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoPersistenciaSimples.Models.Clientes;
+using ProjetoPersistenciaSimples.Services;
+using System.Collections.Generic;
+using System;
+
+public class ClienteController
+{
+    private readonly ClienteService _clienteService;
+
+    public ClienteController()
+    {
+        _clienteService = new ClienteService();
+    }
+
+    public void CadastrarPessoaFisica(string nome, string cpf)
+    {
+        var pessoaFisica = new PessoaFisica
+        {
+            Id = _clienteService.ObterClientes().Count + 1,
+            Nome = nome,
+            CPF = cpf
+        };
+        _clienteService.CadastrarCliente(pessoaFisica);
+        Console.WriteLine("Pessoa Física cadastrada com sucesso.");
+    }
+
+    public void CadastrarPessoaJuridica(string nome, string cnpj)
+    {
+        var pessoaJuridica = new PessoaJuridica
+        {
+            Id = _clienteService.ObterClientes().Count + 1,
+            Nome = nome,
+            CNPJ = cnpj
+        };
+        _clienteService.CadastrarCliente(pessoaJuridica);
+        Console.WriteLine("Pessoa Jurídica cadastrada com sucesso.");
+    }
+
+    public List<Cliente> ListarClientes()
+    {
+        return _clienteService.ObterClientes();
+    }
+}
---
+++ Controller/ImovelController.cs
@@ -0,0 +1,42 @@
+﻿// Controllers/ImovelController.cs
+using ProjetoPersistenciaSimples.Services;
+using System;
+using System.Collections.Generic;
+
+public class ImovelController
+{
+    private readonly ImovelService _imovelService;
+    private readonly ClienteController _clienteController;
+
+    public ImovelController()
+    {
+        _imovelService = new ImovelService();
+        _clienteController = new ClienteController();
+    }
+
+    public void CadastrarImovel(string endereco, string inscricaoIPTU, int proprietarioId)
+    {
+        var proprietario = _clienteController.ListarClientes().Find(c => c.Id == proprietarioId);
+        if (proprietario == null)
+        {
+            Console.WriteLine("Proprietário não encontrado!");
+            return;
+        }
+
+        var imovel = new Imovel
+        {
+            Id = _imovelService.ObterImoveis().Count + 1,
+            Proprietario = proprietario,
+            Endereco = endereco,
+            InscricaoIPTU = inscricaoIPTU
+        };
+
+        _imovelService.CadastrarImovel(imovel);
+        Console.WriteLine("Imóvel cadastrado com sucesso.");
+    }
+
+    public List<Imovel> ListarImoveis()
+    {
+        return _imovelService.ObterImoveis();
+    }
+}
